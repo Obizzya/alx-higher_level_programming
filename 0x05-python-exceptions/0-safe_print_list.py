f05ed2fc24c4dd58449df5a453fbda019d2eae71
Obizzya <ocenarnold@gmail.com>
2022-10-10 19:32:09 +0000

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-
-    for i in range(x):
+    j = 0
+    for i in range(0, x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
-            break
+            my_list[i]
+        except IndexError:
+            print(end='')
         else:
-            count += 1
-
+            j += 1
+            print(my_list[i], end='')
     print()
-    return (count)
+    return j
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    is_int = True
     try:
         print("{:d}".format(value))
-    except:
-        return (False)
-    else:
-        return (True)
+    except (ValueError, TypeError):
+        is_int = False
+    return is_int
